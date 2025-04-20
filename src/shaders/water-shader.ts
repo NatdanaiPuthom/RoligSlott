c97d4b2e3a0a6f44ec29b5cce3d2fe82c050ff3f
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2025-04-20 15:15:01 +0200

    Update water-shader.ts

--- src/shaders/water-shader.ts
+++ src/shaders/water-shader.ts
@@ -2,25 +2,32 @@ import {
   colorToNormal,
   edgeDepthEffect,
   mix,
-  oneMinus,
-  pow,
+  
   rgb,
   rgba,
   Sampler2DNode,
+
   standardMaterial,
   textureSampler2d,
   timeUniforms,
   transformed,
   varying,
+
   vec2,
+
   Vec2Node,
+
+ 
+ 
 } from "@hology/core/shader-nodes"
+
 import {
   NodeShader,
   NodeShaderOutput,
   Parameter,
 } from "@hology/core/shader/shader"
-import { Color, Texture } from "three"
+
+import {Color, Texture} from 'three'
 
 export default class WaterShader extends NodeShader {
   @Parameter()
@@ -31,48 +38,30 @@ export default class WaterShader extends NodeShader {
 
   @Parameter()
   normalMap: Texture = new Texture()
+  
+
+  @Parameter()
+  alpha: number = 0.5 // Default transparency
 
   output(): NodeShaderOutput {
     const depth = edgeDepthEffect(4)
 
-    const gradient = mix(rgb(this.color), rgb(this.shallowColor), depth)
-
-    const opacity = oneMinus(pow(depth, 1.2))
-
-    const color = rgba(gradient, opacity)
+    const gradient = mix(rgb(this.color), rgb(this.shallowColor), depth)  
 
     const worldCoord = varying(transformed.worldPosition.xz)
-
     const normalMap = textureSampler2d(this.normalMap)
-    const normalSample1 = sampleWaveNormal(
-      normalMap,
-      worldCoord,
-      0.01,
-      vec2(-0.4, 0.9),
-      1
-    )
-    const normalSample2 = sampleWaveNormal(
-      normalMap,
-      worldCoord,
-      0.02,
-      vec2(0.5, 0.2),
-      0.4
-    )
-    const normal = colorToNormal(mix(normalSample1, normalSample2, 0.5), 1)
+    const normalSample1 = sampleWaveNormal(normalMap, worldCoord, .0008, vec2(-.4, .9), 9) 
+    const normalSample2 = sampleWaveNormal(normalMap, worldCoord, .002, vec2(.5, .2), 4)
 
+    const normal = colorToNormal(mix(normalSample1,normalSample2,.5) , 7)
     return {
-      color: standardMaterial({ color, normal, roughness: 0.15 }),
+      color: standardMaterial({color:gradient, normal, roughness: 0.6})
     }
   }
-}
 
-function sampleWaveNormal(
-  map: Sampler2DNode,
-  coord: Vec2Node,
-  scale: number,
-  direction: Vec2Node,
-  speed: number
-) {
+
+}
+function sampleWaveNormal(map: Sampler2DNode, coord: Vec2Node, scale: number, direction: Vec2Node, speed: number) {
   const offset = direction.multiplyScalar(timeUniforms.elapsed.multiply(speed))
   return map.sample(coord.add(offset).multiplyScalar(scale))
 }
