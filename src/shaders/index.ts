e66c88a6d9cc78c4f2c83d5fb7505aaf8bc8f522
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2025-04-20 15:13:59 +0200

    Revert "Shaders!"
    This reverts commit 80d11784212ce0702451e875f7480ab284596953.

--- src/shaders/index.ts
+++ src/shaders/index.ts
@@ -1,9 +1,4 @@
 import ExampleShader from './example-shader';
-import RoofShader from './roof-shader';
-import BoulderShader from './boulder-shader';
-import LandscapeShader from './landscape-shader';
-import RockShader from './rock-shader';
-import WoodShader from './wood-shader';
 import WaterShader from './water-shader';
 import GrassTestShader from './grass-test-shader';
 import GrassShader from './grass-shader';
@@ -14,10 +9,5 @@ export default {
   LeavesShader,
   GrassShader,
   GrassTestShader,
-  WaterShader,
-  WoodShader,
-  RockShader,
-  LandscapeShader,
-  BoulderShader,
-  RoofShader
+  WaterShader
 }
--- src/shaders/leaves-shader.ts
+++ src/shaders/leaves-shader.ts
@@ -1,52 +1,65 @@
 
+import { attributes, combineTransforms, dot, float, mix, normalize, rgb, sin, smoothstep, standardMaterial, textureSampler2d, timeUniforms, translateX, translateZ, varyingAttributes, vec2 } from "@hology/core/shader-nodes";
 import { NodeShader, NodeShaderOutput, Parameter } from "@hology/core/shader/shader";
-import {
-  colorToNormal,
-  edgeDepthEffect,
-  mix,
-  
-  rgb,
-  rgba,
-  Sampler2DNode,
-
-  standardMaterial,
-  textureSampler2d,
-  timeUniforms,
-  transformed,
-  varying,
-
-  vec2,
-
-  Vec2Node,
-
- 
-} from "@hology/core/shader-nodes"
-import { Color, Texture} from 'three';
+import { Color, Material, Texture } from 'three';
 
 export default class LeavesShader extends NodeShader {
   @Parameter()
-   color: Color = new Color()
-   
- 
-   
-   @Parameter()
-   normalMap: Texture = new Texture()
-     
- 
-   output(): NodeShaderOutput {
+  color: Color = new Color(0x000000)
+
+  @Parameter()
+  colorTop: Color = new Color(0x000000)
+
+  @Parameter()
+  hasGradient: boolean = false
+
+  @Parameter()
+  gradientStart: number = 0
+
+  @Parameter()
+  gradientEnd: number = 1
+
+  @Parameter()
+  alphaMap: Texture = new Texture()
+
+  @Parameter()
+  windStrength: number = 15
+
+  output(): NodeShaderOutput {
     
- 
-   
- 
+    const opacity = textureSampler2d(this.alphaMap).sample(varyingAttributes.uv).r
+
+    const time = timeUniforms.elapsed
+    const windDirection = normalize(vec2(-0.2, 0.9))
     
-     const worldCoord = varying(transformed.worldPosition.xy)
-        const normalMap = textureSampler2d(this.normalMap)
-        const normalSample = normalMap.sample(worldCoord.multiplyScalar(.2))
-    
-        const normal = colorToNormal(normalSample, 1)
-       
-     
-     return {
-       color: standardMaterial({color: rgb(this.color), normal, roughness: 0.85})
-     }}
-   }
+    const windEffect = sin(dot(attributes.position.xy, windDirection).multiply(10).add(time.multiply(2))).multiply(this.windStrength);
+
+    const gradient = mix(rgb(this.color), rgb(this.colorTop), smoothstep(float(this.gradientStart), float(this.gradientEnd), varyingAttributes.position.y))
+
+    const color = this.hasGradient 
+      ? gradient
+      : rgb(this.color)
+
+    return {
+      color: standardMaterial({color: color.rgba(opacity)}),
+      transform: combineTransforms(
+        translateX(windEffect),
+        translateZ(windEffect.multiply(.5))
+      ),
+      alphaTest: 0.5
+    }
+  } 
+
+  build(): Material {
+    const mat = super.build()
+    /** 
+     * Custom shader normally require a custom depth material. 
+     * However, we can utilize the default behavior by exposing the alpha map 
+     * on the material.
+     * However, it does not currently work with vertex transforms.
+     * @ts-expect-error This property doesn't exist yet on  */
+    mat.alphaMap = this.alphaMap
+    return mat
+  }
+
+}
